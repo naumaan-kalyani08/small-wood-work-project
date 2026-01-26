87713a81db3ffee6346c683590d207978110742a
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-01-26 15:43:39 +0530

    clean up the Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// commit code clean up 
+import React from 'react'
+import CommonStrucutre from './CommonLayouts/CommonStrucutre'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <CommonStrucutre />
+    <h1 className="mt-100 text-3xl font-bold underline bg-red-100">
+    Hello world!
+  </h1>
+  
     </>
   )
 }
