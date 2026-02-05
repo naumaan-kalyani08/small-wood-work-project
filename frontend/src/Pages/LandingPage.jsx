63348c4870cc9aedc478d045b8348d03aef63e13
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-02-05 19:38:09 +0530

    added landing page

---
+++ frontend/src/Pages/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import Products from '../Components/Products'
+import Manufacturing from '../Components/Manufacturing'
+import Contact from '../Components/Contact'
+import Transition from '../Components/Transition'
+
+const LandingPage = () => {
+    return (
+        <>
+            <Hero />
+            <Products />
+            <Manufacturing />
+            <Contact />
+        </>
+    )
+}
+
+export default Transition(LandingPage);
