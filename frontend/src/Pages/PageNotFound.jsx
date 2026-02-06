a01d68e7b4d6280f10c6e63e5ce22368ce9f5fa4
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-02-06 23:03:53 +0530

    added 404 page

---
+++ frontend/src/Pages/PageNotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Transition from '../Components/Transition'
+
+const PageNotFound = () => {
+    return (
+        <div>PageNotFound</div>
+    )
+}
+
+export default Transition(PageNotFound);
