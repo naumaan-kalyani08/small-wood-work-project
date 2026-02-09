bd37879172bcbae328cdd0dd7997e107849c1cc6
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-02-09 21:21:33 +0530

    setup app.js with cleaner approach

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 // commit code clean up 
-import React from 'react'
-import CommonStrucutre from './CommonLayouts/CommonStrucutre'
-
+import SmoothScrollEffect from './Components/SmoothScrollEffect'
+import Router from './Routes/Router'
+import './Style/App.css'
 const App = () => {
   return (
-    <>
-    <CommonStrucutre />
-    <h1 className="mt-100 text-3xl font-bold underline bg-red-100">
-    Hello world!
-  </h1>
-  
-    </>
+    <SmoothScrollEffect>
+      <Router />
+    </SmoothScrollEffect>
   )
 }
 
