50717b9496687c22036ad00e5c8355d70b6ac5fb
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-02-10 23:09:07 +0530

    added products page

---
+++ frontend/src/Pages/ProductsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Transition from '../Components/Transition'
+
+const ProductsPage = () => {
+    return (
+        <div>ProductsPage</div>
+    )
+}
+
+export default Transition(ProductsPage)
