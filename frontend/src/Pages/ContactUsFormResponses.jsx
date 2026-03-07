a148cf447532e214fd8a12bc301ff3ec1a709dc9
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-03-07 15:27:52 +0530

    working on contact us form responses layout

---
+++ frontend/src/Pages/ContactUsFormResponses.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Transition from '../Components/Transition'
+import { ReusableTableStructure } from '../Components/ReusableComponents'
+const ContactUsFormResponses = () => {
+    return (
+        <div>
+            ContactUsFormResponses
+            <ReusableTableStructure />
+        </div>
+    )
+}
+
+export default Transition(ContactUsFormResponses)
