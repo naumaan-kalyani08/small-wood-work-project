c69d3e69981b4230331252142bd65bfe24fe6782
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-03-08 23:22:48 +0530

    updated page 404

--- frontend/src/Pages/PageNotFound.jsx
+++ frontend/src/Pages/PageNotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Transition from '../Components/Transition'
+import { Link } from 'react-router';
 
 const PageNotFound = () => {
     return (
-        <div>PageNotFound</div>
+        <div className='flex-box-center'>
+            <h1 className='text-4xl font-bold text-center'>404 - Page Not Found</h1>
+            <p className='text-center mt-4 text-gray-600'>The page you are looking for does not exist.</p>
+            <p className='text-center mt-4 text-gray-600'>Please check the URL or navigate back to the previous page.</p>
+            <Link to="/" className='block text-center mt-6 text-blue-500 hover:underline'>Go to Home</Link>
+        </div>
     )
 }
 
