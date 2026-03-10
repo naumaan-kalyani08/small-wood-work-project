5359ac1bf11605fe00696b5f7356c72ad7244108
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-03-10 22:25:25 +0530

    working on contac us form responses

--- frontend/src/Pages/ContactUsFormResponses.jsx
+++ frontend/src/Pages/ContactUsFormResponses.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Transition from '../Components/Transition'
 import { ReusableTableStructure } from '../Components/ReusableComponents'
+import { ContactUscolumns, DummyData } from '../Json/fromResponsesColumns'
+
 const ContactUsFormResponses = () => {
+    const [FormData, setFormData] = useState([])
+    const apiUrl = import.meta.env.VITE_APP_API_URL
+
+    const fetchFormData = async () => {
+        try {
+            const response = await fetch(`${apiUrl}contact-submissions`)
+            const data = await response.json()
+            setFormData(data)
+            console.log('Fetched form data:', FormData)
+        } catch (error) {
+            console.error('Error fetching form data:', error)
+        }
+    }
+    useEffect(() => {
+        fetchFormData()
+    }, [])
     return (
-        <div>
+        <div className='bg-white my-2 p-4 rounded-lg shadow-md container mx-auto'>
             ContactUsFormResponses
-            <ReusableTableStructure />
+            {apiUrl}
+            {FormData}
+            <ReusableTableStructure dataSource={DummyData} columns={ContactUscolumns} rowKey="key" />
         </div>
     )
 }
