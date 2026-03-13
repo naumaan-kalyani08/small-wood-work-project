22e2d6591503c6800334111b56285549e1e185bb
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-03-13 17:07:33 +0530

    added coliums json for contact form

---
+++ frontend/src/Json/fromResponsesColumns.js
@@ -0,0 +1,47 @@
+export const ContactUscolumns=[
+    {
+    title: 'Name',
+    dataIndex: 'full_name',
+    key: 'full_name',
+    },
+    {
+    title: 'First Name',
+    dataIndex: 'first_name',
+    key: 'first_name',
+    },
+    {
+    title: 'Last Name',
+    dataIndex: 'last_name',
+    key: 'last_name',
+    },
+    {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+    },
+    {
+    title: 'Phone Number',
+    dataIndex: 'phone_number',
+    key: 'phone_number',
+    },
+    {
+    title: 'Message',
+    dataIndex: 'message',
+    key: 'message',
+    },
+   
+];
+export const DummyData = [
+  {
+    key: 1,
+    name: "John",
+    age: 28,
+    address: "London"
+  },
+  {
+    key: 2,
+    name: "Mike",
+    age: 32,
+    address: "New York"
+  }
+];
