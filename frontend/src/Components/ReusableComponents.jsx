5ef89f9f99958f1edb4c98afbc02d458def341cc
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2026-04-16 21:47:09 +0530

    updated reusable input fields

--- frontend/src/Components/ReusableComponents.jsx
+++ frontend/src/Components/ReusableComponents.jsx
@@ -75,7 +75,7 @@ export const ReusableCardLayout = () => {
         </div>
     )
 }
-export const ReusableInputField = ({ label, placeholder, type = 'text', inputSize = '', additionLabelClass = '', additionalInputClass = '' }) => {
+export const ReusableInputField = ({ value, onchange, label, placeholder, type = 'text', inputSize = '', additionLabelClass = '', additionalInputClass = '', name = '' }) => {
     return (
         <div className={`mb-4 ${inputSize}`}>
             <label className={`block text-gray-700 text-sm font-bold mb-2 ${additionLabelClass}`} htmlFor={label}>
@@ -84,8 +84,11 @@ export const ReusableInputField = ({ label, placeholder, type = 'text', inputSiz
             <input
                 className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${additionalInputClass}`}
                 id={label}
+                name={name}
                 type={type}
                 placeholder={placeholder}
+                value={value}
+                onChange={onchange}
             />
         </div>
     );
