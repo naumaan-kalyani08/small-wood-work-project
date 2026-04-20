68daa7a62aaa87e404bd1b821e4abdfcaf5b4a1c
Naumaan <naumaan.kalyani02@gmail.com>
2026-04-20 21:29:02 +0530

    Added Delete common function

--- frontend/src/CommonUtilities/CommonFunctions.js
+++ frontend/src/CommonUtilities/CommonFunctions.js
@@ -79,3 +79,44 @@ export const PostCommonFunction = async (apiEndpoint,body,showMessage = true) =>
         error: errorMessage
         }  
 }}
+
+export const deleteCommonFUnction = async (apiEndpoint,parameters ={},showMessage = true) => {
+    try {
+        console.log('API Endpoint:', apiEndpoint);
+        const baseUrl = import.meta.env.VITE_APP_API_URL;
+        console.log('Base URL:', baseUrl);
+        const apiUrl = new URL(`${baseUrl}${apiEndpoint}`);
+        Object.keys(parameters).forEach(key => {
+            apiUrl.searchParams.append(key, parameters[key])
+        });
+        const response = await fetch(apiUrl,{
+            method: 'DELETE',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+        });
+          const data = await response.json();
+        if (!response.ok) {
+            const errorMessage = data.message || 'An error occurred while fetching data.';
+            if (showMessage) message.error(errorMessage);
+            return {
+                success: false,
+                error: errorMessage
+            };                       
+        }
+        if (showMessage && data.message) {
+            message.success(data.message);
+        }
+        return{
+            success: true,
+            data 
+        }
+    } catch (error) {
+        console.log(error)
+        if(showMessage) message.error(errorMessage)
+        return{
+        success: false,
+        error: errorMessage
+        }
+    }
+}
