82af0cc067f2edb1c437e56fadf85f2157c6d148
Naumaan <naumaan.kalyani02@gmail.com>
2026-04-30 21:45:23 +0530

    added api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
 Route::post('/contact', [ContactController::class, 'submittedContactForm']);
 Route::get('/contact-submissions', [ContactController::class, 'getContactFormSubmissions']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
