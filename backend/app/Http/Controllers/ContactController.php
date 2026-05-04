5c2af7eab4d1afb68d5771501861381c9e991c6f
Naumaan <naumaan.kalyani02@gmail.com>
2026-05-04 22:54:01 +0530

    updated contact controller

--- backend/app/Http/Controllers/ContactController.php
+++ backend/app/Http/Controllers/ContactController.php
@@ -2,25 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactFormRequest;
 use App\Models\ContactUs;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class ContactController extends Controller
 {
-    public function submittedContactForm(Request $request)
+    public function submittedContactForm(ContactFormRequest $request)
     {
-        Log::info('function triggered');
-        // Add your contact form submission logic here
-        $validated = $request->validate([
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'full_name' => 'nullable|string|max:255',
-            'phone_number' => 'nullable|string|max:20',
-            'email' => 'required|email',
-            'message' => 'required|string',
-        ]);
-
+        $validated = $request->validated();
         ContactUs::create($validated);
 
         return response()->json(['message' => 'Contact form submitted successfully'], 201);
@@ -28,11 +17,11 @@ class ContactController extends Controller
 
     public function getContactFormSubmissions()
     {
-        // Retrieve all contact form submissions
         $submissions = ContactUs::all();
+
         return response()->json([
-            'data' => $submissions, 
-            'message' => 'Contact form submissions retrieved successfully'
+            'data' => $submissions,
+            'message' => 'Contact form submissions retrieved successfully',
         ]);
     }
 }
