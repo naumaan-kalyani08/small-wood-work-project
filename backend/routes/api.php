e6a68c09cb454b062a4f9d9d20f1e9a3c15dcdea
Naumaan <naumaan.kalyani02@gmail.com>
2026-05-07 22:33:25 +0530

    updated api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
 
-Route::post('/contact', [ContactController::class, 'submittedContactForm']);
-Route::get('/contact-submissions', [ContactController::class, 'getContactFormSubmissions']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('throttle:60,1')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/contact', [ContactController::class, 'submittedContactForm']);
+});
+
+Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
+    Route::get('/contact-submissions', [ContactController::class, 'getContactFormSubmissions']);
+});
