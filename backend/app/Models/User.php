5c06e0f76b192c364cd5bd9f7218ac64757f900d
Naumaan <naumaan.kalyani02@gmail.com>
2026-05-08 22:11:23 +0530

    updated User model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -18,19 +18,18 @@ class User extends Authenticatable
         'email',
         'password',
         'dob',
-        'number'
+        'number',
     ];
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-            'dob' => 'date',
-            'number' => 'integer',
-        ];
-    }
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'dob' => 'date',
+        'number' => 'integer',
+    ];
 }
