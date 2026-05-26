b3c64e0c343d1117987965c062401ad6fca2d048
Naumaan <naumaan.kalyani02@gmail.com>
2026-05-26 22:19:18 +0530

    Daily commit

--- backend/app/Models/ContactUs.php
+++ backend/app/Models/ContactUs.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class ContactUs extends Model
 {
     protected $table = 'contact_form_data';
+
     protected $fillable = [
         'first_name',
         'last_name',
         'full_name',
         'phone_number',
+        'company',
         'email',
         'message',
     ];
