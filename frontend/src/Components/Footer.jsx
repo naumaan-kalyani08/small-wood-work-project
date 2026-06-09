2a56db0619b2d632916c339d2fd0329218f98571
Naumaan <naumaan.kalyani02@gmail.com>
2026-06-09 21:38:17 +0530

    updated footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Facebook, Twitter, Linkedin, Instagram } from "lucide-react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
