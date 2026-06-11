3d89951f0a4100bfd8f8d3753ac6a53565869e1d
Naumaan <naumaan.kalyani02@gmail.com>
2026-06-11 21:50:25 +0530

    updated footer

--- frontend/src/Pages/ContactUsFormResponses.jsx
+++ frontend/src/Pages/ContactUsFormResponses.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router-dom'
 import Transition from '../Components/Transition'
 import { ReusableTableStructure } from '../Components/ReusableComponents'
 import { ContactUscolumns, DummyData } from '../Json/fromResponsesColumns'
