edfa479d3c4b6e9fe77b333be9b4452d67e5e386
Naumaan <naumaan.kalyani02@gmail.com>
2026-06-13 18:43:50 +0530

    Updated ROuter

--- frontend/src/Pages/PageNotFound.jsx
+++ frontend/src/Pages/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Transition from '../Components/Transition'
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
     return (
