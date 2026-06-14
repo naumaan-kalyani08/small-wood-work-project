144bae9651c5691b9e98dc69010973e005ec8e9a
Naumaan <naumaan.kalyani02@gmail.com>
2026-06-14 23:26:11 +0530

    Updated Auth Page

--- frontend/src/Pages/AuthPage.jsx
+++ frontend/src/Pages/AuthPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import { message } from 'antd';
 import Transition from '../Components/Transition';
 import { ReusableInputField } from '../Components/ReusableComponents';
